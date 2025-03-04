037a219494968b5f8efb09121baa205546bbef39
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 14:49:13 +0300

    declare the distanceFromHqInFeet function

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const { use } = require("chai");
 
 // Code your solution in this file!
 function distanceFromHqInBlocks(userlocation) {
-  //   return userlocation - 42;
-  //   let dstnce = Math.abs(userlocation);
   if (userlocation <= 42) {
     return 42 - userlocation;
   } else {
     return userlocation - 42;
+    //  userBlocks;
   }
+}
+
+function distanceFromHqInFeet() {
+  //   return userBlocks * 264;
 }
