fea1e4b87c3aa5b2f76d30fed0561ced132c41cf
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 14:33:30 +0300

    rewrite the distanceFromHqInBlocks function using an if else statement

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const { use } = require("chai");
 
 // Code your solution in this file!
 function distanceFromHqInBlocks(userlocation) {
-  return 42 - userlocation;
+  //   return userlocation - 42;
   //   let dstnce = Math.abs(userlocation);
+  if (userlocation <= 42) {
+    return 42 - userlocation;
+  } else {
+    return userlocation - 42;
+  }
 }
