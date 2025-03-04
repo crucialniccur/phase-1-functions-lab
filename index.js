eb81d95404c7c54fa7c0d7b4b30a41c1dfdd0998
Mati Joseph <crucialniccur@gmail.com>
2025-03-04 14:27:44 +0300

    create the function distanceFromHqInBlocks

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const { use } = require("chai");
+
 // Code your solution in this file!
+function distanceFromHqInBlocks(userlocation) {
+  return 42 - userlocation;
+  //   let dstnce = Math.abs(userlocation);
+}
